cbfd9437eeb4924bcfd743c4307d630201f18a7d
Wesley Heron <wfarias.ti@gmail.com>
2020-05-02 12:45:45 -0300

    disable node_modules cache

--- api/app/services/email-service.js
+++ api/app/services/email-service.js
@@ -5,8 +5,8 @@ let transporter = nodemailer.createTransport({
     service: 'gmail',
     secure: true,
     auth: {
-        user: 'vanillacode2020@gmail.com',
-        pass: 'megahack2020'
+        user: 'equipevanillacode@gmail.com',
+        pass: 'MegaHack2020#'
     }
 });
 
@@ -15,7 +15,7 @@ let transporter = nodemailer.createTransport({
 exports.send =  (mailOptions, err, res) => {
     transporter.sendMail({
         to: mailOptions.to,
-        from: 'Equipe VanillaCode <vanillacode2020@gmail.com>',
+        from: 'Equipe VanillaCode <equipevanillacode@gmail.com>',
         subject: mailOptions.subject,
         text: mailOptions.body,
         html: mailOptions.html
